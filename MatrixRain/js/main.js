644fc43aa3acf92cadec47467e401e59fb6c763f
Jonathan Craven <jcraven42@gmail.com>
2017-04-11 21:38:44 +0000

    made kanjis blink

--- MatrixRain/js/main.js
+++ MatrixRain/js/main.js
@@ -1,35 +1,52 @@
-var charArr;
+var chars;
 var ctx;
-var randCharX;
-var randCharY;
-// 9 wide, 6 tall grid of 24x24px icons
-var indexArr = [];
+var kanjiArr=[];
+var freq = 100;
 
 function preload() {
-    charArr = loadImage("chars.png");
+    chars = loadImage("chars.png");
+    charsWhite = loadImage("charsWhite.png");
 }
 
 function setup(){
   createCanvas(window.innerWidth, window.innerHeight);
-  noLoop();
+  // for (var i = 0; i < freq; i++) {
+  //   var randposX = floor(random(width/24))*24;
+  //   var randposY = floor(random(height/24))*24;
+  //   var pos = createVector(randposX,randposY);
+  //   var randkanX = floor(random(9))*24;
+  //   var randkanY = floor(random(6))*24;
+  //   var kan = createVector(randkanX,randkanY);
+  
+  //   kanjiArr.push(new Kanji(pos,kan));
+  // }
+  
+  //noLoop();
 }
 
 function draw(){
   background(0);
-  for (var i = 0; i < 100; i++) {
-
-    randCharX = floor(random(9))*24;
-    randCharY = floor(random(6))*24;
-    randposX = floor(random(width/24))*24;
-    randposY = floor(random(height/24))*24;
-    drawChar(randposX,randposY,randCharX,randCharY);
+  kanjiArr.push(new Kanji());
+  var len = kanjiArr.length;
+  if (len>0){
+    for (var i = len-1; i >= 0; i--) {
+      //if(kanjiArr[i]!=null){
+        kanjiArr[i].render();
+        kanjiArr[i].tick();
+      //}
+      if (!kanjiArr[i].valid){
+            kanjiArr.splice(i, 1);
+          }
+    }
+  
   }
 }
 
-function drawChar(x, y, charX, charY){
-  image(charArr,
-      x, y,
-      24, 24,
-      charX, charY,
-      24, 24);
+function update(){
+  // var len = kanjiArr.length;
+  //   for (var i = 0; i < len; i++) {
+  //   kanjiArr[i].update();
+  // }
 }
+
+
